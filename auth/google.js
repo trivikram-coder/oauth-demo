c57de0a71caca7d571e301a8ee8f535f53478f19
vikram-42-alla <allatrivikram@gmail.com>
2026-01-16 09:36:26 +0530

    commit

--- auth/google.js
+++ auth/google.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/callback"
+  callbackURL: "https://oauth-demo-pltj.onrender.com/auth/google/callback"
 }, (accessToken, refreshToken, profile, done) => {
   return done(null, profile);
 }));
